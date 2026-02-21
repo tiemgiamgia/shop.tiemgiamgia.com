cb9b37f4fc4a00c825b0105486f94cae613ada51
tiemgiamgia <157579121+tiemgiamgia@users.noreply.github.com>
2026-02-21 11:09:05 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,7 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-
-  output: "server",          // 🔥 SSR MODE
-
-  adapter: cloudflare(),     // 🔥 QUAN TRỌNG NHẤT
-
-  site: "https://shop.tiemgiamgia.com"
-
+  site: "https://shop.tiemgiamgia.com",
+  output: "server",          // ✅ SSR
+  adapter: cloudflare()      // ✅ Cloudflare adapter
 });

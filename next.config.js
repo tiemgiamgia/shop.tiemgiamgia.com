22e1082d745a883f40d9e7b412e38744c8b18af6
tiemgiamgia <157579121+tiemgiamgia@users.noreply.github.com>
2026-02-21 22:06:52 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-import { withCloudflare } from "@cloudflare/next-on-pages";
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-
   reactStrictMode: true,
 
   images: {
-    unoptimized: true   // 🔥 Bắt buộc nếu dùng ảnh ngoài (Shopee CDN)
+    unoptimized: true
   }
-
 };
 
-export default withCloudflare(nextConfig);
+export default nextConfig;

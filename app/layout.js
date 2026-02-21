a5c8a2f89de3e0abe1f99858da8f7ec73ee3f70d
tiemgiamgia <157579121+tiemgiamgia@users.noreply.github.com>
2026-02-21 21:23:16 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import "./site.css";
+
+export const metadata = {
+  title: "Tiệm Giảm Giá",
+  description: "Sản phẩm Shopee giá tốt"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="vi">
+      <body>
+        {children}
+        <script src="/js/sticky-bar.js" defer></script>
+      </body>
+    </html>
+  );
+}

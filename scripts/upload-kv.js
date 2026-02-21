4909e8bb904c5cbb3be1a59d6f0e0e7927a2fad1
tiemgiamgia <157579121+tiemgiamgia@users.noreply.github.com>
2026-02-21 18:00:30 +0700

    Update upload-kv.js

--- scripts/upload-kv.js
+++ scripts/upload-kv.js
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 console.log("🔥 Uploading KV...");
 
 execSync(
-  `wrangler kv:bulk put --binding=PRODUCTS kv-data.json`,
+  `wrangler kv:bulk put --namespace-id=ab8b34413f26438bbe2122a4d128927b kv-data.json`,
   { stdio: "inherit" }
 );
 
